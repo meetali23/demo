66667ee2474e869f41aa43d65baccf618dea0773
meetali23 <44087807+meetali23@users.noreply.github.com>
2018-10-12 16:10:58 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+class Sum
+{
+	public static void main(String args[])
+	{
+	
+	System.out.println("Enter two numbers");
+	Scanner sc=new Scanner(System.in);
+	int a,b,c;
+	a=sc.nextInt();
+	b=sc.nextInt();
+	c=a+b;
+	System.out.println("The sum is" +c);
+	}
+}
+	
